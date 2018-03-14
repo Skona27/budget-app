b97dcd944645c206c676f9d8140f7c6379e6caba
Jakub Skoneczny <skoneczny.j@gmail.com>
2018-03-14 11:10:41 +0100

    getting data from input works

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
 // BUDGET CONTROLLER
-var budgetController = (function() {
+var budgetController = (function () {
 
 
 })();
 
 
 // UI CONTROLLER
-var UIController = (function() {
+var UIController = (function () {
 
+    // list of all dom elements
+    var DOM = {
+        addBtn: document.querySelector(".add__btn"),
+        inputType: document.querySelector(".add__type"),
+        inputDesc: document.querySelector(".add__description"),
+        inputValue: document.querySelector(".add__value"),
+    };
 
+    // return to the public
+    return {
+        // return list of dom elements
+        getDOM: function () { return DOM; },
+        // return data from the input
+        getInput: function () {
+            return {
+                type: DOM.inputType.value,
+                description: DOM.inputDesc.value,
+                value: DOM.inputValue.value,
+            };
+        },
+    };
 })();
 
 
 // APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
 
-    var ctrlAddItem = function() {
+    // get the list of all dom elements
+    var DOM = UICtrl.getDOM();
+
+    var ctrlAddItem = function () {
         // get the input
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // add the item to the budget controller
 
@@ -30,12 +55,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     // event listeners for adding new item
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    DOM.addBtn.addEventListener("click", ctrlAddItem);
 
-    document.addEventListener("keypress", function(event) {
+    document.addEventListener("keypress", function (event) {
         // check for enter keypress
-        if(event.keyCode === 13) {
-           ctrlAddItem();
+        if (event.keyCode === 13) {
+            ctrlAddItem();
         }
     });
 
